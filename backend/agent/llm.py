ac8f33b46f40155d89102d2decb0389280c85b9c
Chothani Dharmik <dharmikchothani59@gmail.com>
2026-08-19 12:34:29 +0530

    .

--- backend/agent/llm.py
+++ backend/agent/llm.py
@@ -1,28 +1,30 @@
 import os
 from dotenv import load_dotenv
+
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
+
 load_dotenv()
 
 # ----------------------------------------------------
 # Primary LLM: Google Gemini
 # ----------------------------------------------------
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash-lite",
-    # or "gemini-1.5-flash" / "gemini-2.0-flash" depending on your API support
-  
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0,
 )
 
 # ----------------------------------------------------
-# Secondary LLM
+# Secondary LLM: Groq
 # ----------------------------------------------------
-
 
 llm2 = ChatGroq(
     model="openai/gpt-oss-120b",
     temperature=1,
     max_tokens=2048,
     top_p=1,
-    reasoning_effort="medium",  # Passed directly as a top-level parameter
+    reasoning_effort="medium",
     api_key=os.getenv("GROQ_API_KEY"),
 )
